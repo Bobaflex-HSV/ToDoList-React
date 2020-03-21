9e1791a6d9cc20c07dc2bd1a68d418fba9e1c26c
Bobaflex-HSV <61234904+Bobaflex-HSV@users.noreply.github.com>
2020-03-21 16:37:05 +0100

    integrate function buttons into description

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -10,21 +10,23 @@ export default function Todo({ value, dateProperty, descriptionProperty }) {
     <div onClick={markAsDone} className="todo-wrapper">
       <div className="todo-title-date">
         <h5>{value}</h5>
-        <span>{dateProperty}</span>
+        <span>Created on: {dateProperty}</span>
       </div>
-      <div className="todo-description">
-        <span>{descriptionProperty}</span>
-      </div>
-      <div className="function-buttons">
-        <button type="button" className="btn btn-secondary button-todo">
-          Edit
-        </button>
-        <button type="button" className="btn btn-secondary button-todo">
-          Mark Done
-        </button>
-        <button type="button" className="btn btn-secondary button-todo">
-          Delete
-        </button>
+      <div className="description-wrapper">
+        <div className="todo-description">
+          <span>{descriptionProperty}</span>
+        </div>
+        <div className="function-buttons">
+          <button type="button" className="btn btn-secondary button-todo">
+            Edit
+          </button>
+          <button type="button" className="btn btn-secondary button-todo">
+            Mark Done
+          </button>
+          <button type="button" className="btn btn-secondary button-todo">
+            Delete
+          </button>
+        </div>
       </div>
     </div>
   );
